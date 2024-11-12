5517699438cb9f0fbfdfb0573b57fc7297506ea1
Bibz-a <u2024260@giki.edu.pk>
2024-11-12 21:59:23 +0500

    menu added

--- proj.cpp
+++ proj.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main(){
+    int choice;
+    cout<<"FLASHCARD QUIZ PROGRAM"<<endl;
+    cout<<"Which action do you want to perform: 1 - ADD A FLASHCARD , 2 - DELETE A FLASHCARD , 3-RANDOM QUIZ , 4-EXIT"<<endl;
+    do{
+        cin>>choice;
+        switch(choice){
+        case 1:{
+        //addflashcard();
+        break;
+        }
+        case 2:{
+        //deleteflashcard();
+        break;
+        }
+        case 3:{
+            //quiz();
+        }
+        default:
+        cout<<"ERROR INVALID INPUT."<<endl;
+    }
+    }while(choice!=4);
     
     return 0;
 }
